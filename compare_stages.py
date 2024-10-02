b752caad3f2577eee8703bef8f837ba606b497c8
Elouan Colybes <118377913+elouan50@users.noreply.github.com>
2024-10-02 16:22:02 +0200

    Corrections (#3)
    * Make it prettier
    
    * Fix wrong control command
    
    * Make description better

--- compare_stages.py
+++ compare_stages.py
@@ -5,7 +5,7 @@ import torch
 import util
 from net.huffmancoding import huffman_encode_model
 
-parser = argparse.ArgumentParser(description='This program compares all stages of this deep compression preocedure.')
+parser = argparse.ArgumentParser(description='This program compares all stages of the deep compression procedure.')
 parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
 args = parser.parse_args()
 
@@ -15,28 +15,43 @@ model = torch.load("saves/initial_model.ptmodel")
 print('Initial model:', type(model).__name__)
 util.print_model_parameters(model)
 
+print(' ')
 print('-'*100)
-print('***After initial training***')
+print(' ')
+print(' '*10+'***After initial training***')
+print(' ')
 util.test(model, use_cuda)
 
 
+print(' ')
 print('-'*100)
-print('***After pruning***')
+print(' ')
+print(' '*10+'***After pruning***')
+print(' ')
 model = torch.load("saves/model_after_retraining.ptmodel")
 util.print_nonzeros(model)
 
 
+print(' ')
 print('-'*100)
-print('***After retraining***')
+print(' ')
+print(' '*10+'***After retraining***')
+print(' ')
 util.test(model, use_cuda)
 
 
+print(' ')
 print('-'*100)
-print('***After quantizing***')
+print(' ')
+print(' '*10+'***After quantizing***')
+print(' ')
 model = torch.load("saves/model_after_weight_sharing.ptmodel")
 util.test(model, use_cuda)
 
 
+print(' ')
 print('-'*100)
-print('***After Huffman encoding***')
+print(' ')
+print(' '*10+'***After Huffman encoding***')
+print(' ')
 huffman_encode_model(model, stats=True)
